480c880523b39f1b5e7ab4455029ebe8230e9f66
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-11-29 12:22:56 +0100

    exercise 12

--- esercizio11/exercise-1.js
+++ esercizio11/exercise-1.js
@@ -1,9 +1,5 @@
-const sum = (a, b) => {
-  return a + b;
-}
+const sum = (a, b) => a + b;
 
-const log = value => {
-  console.log(value);
-}
+const log = value => console.log(value);
 
 log(sum(2, 5));
