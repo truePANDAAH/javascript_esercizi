53abd2bfea40d32f4ac57dada8bb388f44cbc275
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-01 19:42:08 +0100

    exercise 25

--- esercizio25/exercise-1.js
+++ esercizio25/exercise-1.js
@@ -1,35 +1,48 @@
 const person = {
-  get firstName () {
-    return this.firstName;
+  get firstName() {
+    return this._firstName;
   },
-  set firstName (name) {
-    this.firstName = name;
-  },
-
-  get lastName () {
-    return this.lastName;
-  },
-  set lastName (name) {
-    this.lastName = name;
+  set firstName(name) {
+    this._firstName = name;
   },
 
-  fullName () {
+  get lastName() {
+    return this._lastName;
+  },
+  set lastName(name) {
+    this._lastName = name;
+  },
+
+  fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
+//alternativa che avevo fatto e non funzionava
+/*
 const john = {
   firstName: person.firstName = 'John',
   lastName: person.lastName = 'Doe',
-  fullName: person.fullName(),
+  fullName() { return person.fullName() },
 }
 
 const simon = {
   firstName: person.firstName = 'Simon',
   lastName: person.lastName = 'Collins',
-  fullName: person.fullName(),
+  fullName() { return person.fullName() },
 }
+*/
 
+// alternativa dopo suggerimento di usare create
+const john = Object.create(person);
+john.firstName = 'John';
+john.lastName = 'Doe';
+
+const simon = Object.create(person);
+simon.firstName = 'Simon';
+simon.lastName = 'Collins';
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
+
+// console.log(person.fullName());
