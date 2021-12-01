7ed9f02fe9a94e78ecbc68d45ada0e6a46161f6a
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-01 18:34:34 +0100

    exercise 17

--- esercizio17/exercise-4.js
+++ esercizio17/exercise-4.js
@@ -1,15 +1,29 @@
-let objCalc;
+function calculate(y) {
+  let x = 0;
 
-function calculate() {
-  objCalc = {
-    add() { x + y },
-    sub() { x - y },
-    multiply() { x * y },
-    divide() { x / y },
-    printResult() { return x }
-  }
-
+  return {
+    add(y) {
+      x += y;
+      return this;
+    },
+    sub(y) {
+      x -= y;
+      return this;
+    },
+    multiply(y) {
+      x *= y;
+      return this;
+    },
+    divide(y) {
+      x /= y;
+      return this;
+    },
+    printResult() {
+      console.log(x);
+    },
+  };
 };
 
 const calculator = calculate();
-calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult();
+// Il risultato sarà: 7
