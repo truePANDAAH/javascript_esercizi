56ee0c52aefa3f128aeb87eea97bb5d244eacf5a
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-07 18:05:37 +0100

    add exercise 37

--- esercizio37/exercise-4.js
+++ esercizio37/exercise-4.js
@@ -1,3 +1,27 @@
+class BankAccount {
+    
+    constructor(money) {
+        this.money = money;
+    }
+
+    deposit(add) {
+        this.money += add;
+        console.log(`account balance after deposit ${this.money}`)
+    }
+
+    withdraw(sub) {
+        this.money -= sub;
+        console.log(`account balance after withdraw ${this.money}`)
+    }
+
+    view() {
+        console.log(this.money);
+    }
+}
+
+
+
+
 const bankAccount = new BankAccount(1000);
 bankAccount.deposit(500);
 bankAccount.deposit(200);
