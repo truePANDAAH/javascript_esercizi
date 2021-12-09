ea4dfe3fb5d5e2c8daa3be6ee93ec27557f0910e
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-09 11:35:32 +0100

    add exercise 40

--- esercizio40/exercise-7.js
+++ esercizio40/exercise-7.js
@@ -1,9 +1,42 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(name) {
+    this._firstName = name;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(surname) {
+    this._lastName = surname;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(years) {
+    this._age = years;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 const person = new Person('Mario', 'Rossi', 25);
 console.log(person.fullName);
+console.log(person.age);
 
 person.firstName = 'Maria';
 person.lastName = 'Verdi';
