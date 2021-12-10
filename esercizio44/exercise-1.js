e4026a6367e9d3b24ffddfc4abd5f896eb148907
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-10 17:51:11 +0100

    fix exercise 44

--- esercizio44/exercise-1.js
+++ esercizio44/exercise-1.js
@@ -11,6 +11,6 @@ console.log('After swap: ', num1, num2); // After swap: 20 10
 
 
 //Destructuring
-[num1, num2] = [10, 20];
+[num1, num2] = [num2, num1];
 
 console.log(`After Destructuring: ${num1} ${num2}`);
