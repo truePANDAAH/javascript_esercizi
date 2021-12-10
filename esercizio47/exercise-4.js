38baef854e430577c325b9b6e67fe1ca351a223e
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-10 18:40:01 +0100

    exercise 47

--- esercizio47/exercise-4.js
+++ esercizio47/exercise-4.js
@@ -5,6 +5,10 @@ const person = {
   age: 25
 };
 
-for (let item of Object.entries(person)) {
-  console.log(`${item[0]}: ${item[1]}`);
+// for (let item of Object.entries(person)) {
+//   console.log(`${item[0]}: ${item[1]}`);
+// }
+
+for (let [key, value] of Object.entries(person)) {
+  console.log(`${key}: ${value}`);
 }
