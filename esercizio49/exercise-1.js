478a0da237adf89636d55936880e9d1b2ed7e247
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-10 18:50:15 +0100

    exercise 49

--- esercizio49/exercise-1.js
+++ esercizio49/exercise-1.js
@@ -7,5 +7,17 @@ class Person {
   }
 }
 
-const developer = new Person(1, 'Mario', 'Rossi', 25);
+//const developer = new Person(1, 'Mario', 'Rossi', 25);
+
 // Print developer as json object
+const developer = JSON.stringify(new Person(1, 'Mario', 'Rossi', 25));
+console.log(typeof developer);
+console.log(developer);
+
+
+/* alternativa lasciando developer invariato
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+let json = JSON.stringify(developer);
+console.log(json);
+*/
