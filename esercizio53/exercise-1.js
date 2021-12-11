ed78f44c5b784cd8bff8407c7fd04b8a070c7a3c
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-11 18:22:28 +0100

    exercise 53

--- esercizio53/exercise-1.js
+++ esercizio53/exercise-1.js
@@ -1,5 +1,16 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+
+function sum(...numbers) {
+    let tot = 0;
+
+    for (number of numbers) {
+        tot += number;
+    }
+
+    return tot;
 }
 
 console.log(sum(1, 2, 3, 4, 5));
